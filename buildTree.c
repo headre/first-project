801dc028f2f39e576f093098651895c9da90ba25
headre <45737374+headre@users.noreply.github.com>
2018-12-13 20:56:16 +0800

    addOrRemove() is added
    Function addOrRemove() is available.

--- buildTree.c
+++ buildTree.c
@@ -37,3 +37,54 @@ void growTree( Node *parent ){
         }
     return;
 }
+void RemoveChildren( Node *node){
+    for (int i=0;i<4;i++){
+        free(node->child[i]);
+        node->child[i]=NULL;
+    }
+    return;
+}
+
+int getFlag(Node *node,double time){
+    int flag;
+    double value;
+    value=nodeValue(node, time);
+    if (value>0.5){
+        flag=1;
+    }
+    else if (value<-0.5){
+        flag=-1;
+    }
+    else{
+        flag=0;
+    }
+    return flag;
+}
+void addOrRemove( Node *node, double time){
+    for (int i=0;i<4;i++){
+        int f=getFlag(node->child[i], time);
+        if (f==1){
+            makeChildren(node->child[i]);
+        }
+        if (f==-1){
+            RemoveChildren(node);
+            break;
+        }
+    }
+    
+    return;
+}
+void adapt( Node *head){
+    if (head->child[0]!=NULL&&head->child[0]->child[0]==NULL){
+        addOrRemove(head, 0.0);
+    }
+    else if (head->child[0]!=NULL&&head->child[0]->child[0]!=NULL){
+        for (int i=0;i<4;i++){
+            adapt(head->child[i]);
+        }
+        
+    }
+    
+    return;
+}
+
--- buildTree.h
+++ buildTree.h
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include "tree-Structure.h"
 #include <stdlib.h>
+#include <math.h>
 Node *makeNode( double x, double y, int level ) {
     
     int i;
@@ -28,5 +29,21 @@ Node *makeNode( double x, double y, int level ) {
     
     return node;
 }
+double value( double x, double y, double time ) {
+    
+    return( 2.0*exp(-8.0*(x-time)*(x-time)) - 1.0 ) ;
+}
+
+double nodeValue( Node *node, double time ) {
+    
+    int level = node->level;
+    double x = node->xy[0];
+    double y = node->xy[1];
+    
+    double h = pow(2.0,-level);
+    
+    return( value( x+0.5*h, y+0.5*h, time ) );
+}
+
 #endif /* buildTree_h */
 
